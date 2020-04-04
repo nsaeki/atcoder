916b8f3e8910db5791e331afaa8bd3b257808c78
Nobuo Saeki <nobuo.saeki@gmail.com>
2020-04-04 18:23:20 +0900

    Modify library for GCD and LCM

--- lib/gcd.go
+++
@@ -1,9 +0,0 @@
-func gcd(a, b int) int {
-	if a > b {
-		a, b = b, a
-	}
-	for a != 0 {
-		a, b = b%a, a
-	}
-	return b
-}
--- lib/lcm.go
+++ lib/lcm.go
@@ -1,34 +1,13 @@
-func divisors(n int) map[int]int {
-	for i := 2; i <= n; i++ {
-		if n%i == 0 {
-			d := divisors(n / i)
-			d[i]++
-			return d
-		}
-	}
-	return make(map[int]int)
-}
-
-func lcm(a, b int) int {
+func gcd(a, b int) int {
 	if a > b {
 		a, b = b, a
 	}
-	if b%a == 0 {
-		return b
+	for a != 0 {
+		a, b = b%a, a
 	}
+	return b
+}
 
-	d := divisors(a)
-	for k, v := range divisors(b) {
-		if v > d[k] {
-			d[k] = v
-		}
-	}
-
-	lcm := 1
-	for k, v := range d {
-		for i := 0; i < v; i++ {
-			lcm *= k
-		}
-	}
-	return lcm
+func lcm(a, b int) int {
+	return a / gcd(a, b) * b
 }
