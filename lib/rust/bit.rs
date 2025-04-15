f049c9307fbfa2704660ec5033853c4a3ea24471
Nobuo Saeki <nobuo.saeki@gmail.com>
2025-04-16 00:41:13 +0900

    Fix BIT in rust

--- lib/rust/bit.rs
+++ lib/rust/bit.rs
@@ -1,21 +1,18 @@
-// WIP
 // BIT (A Binary indexed tree or Fenwick tree)
 struct BIT {
-    n: usize,
     data: Vec<i32>,
 }
 
 impl BIT {
     fn new(n: usize) -> Self {
         Self {
-            n: n,
             data: vec![0; n + 1],
         }
     }
 
     fn add(&mut self, mut i: usize, v: i32) {
         i += 1;
-        while i <= self.n {
+        while i < self.data.len() {
             self.data[i] += v;
             i += i & i.wrapping_neg();
         }
@@ -23,7 +20,6 @@ impl BIT {
 
     fn sum(&self, mut i: usize) -> i32 {
         let mut v = 0;
-        i += 1;
         while i > 0 {
             v += self.data[i];
             i -= i & i.wrapping_neg();
@@ -40,8 +36,11 @@ mod tests {
     fn sum() {
         let mut bit = BIT::new(3);
         for i in 0..3 {
-            bit.add(i, i as i32);
+            bit.add(i, i as i32 + 1);
         }
+        assert!(bit.sum(0) == 0);
+        assert!(bit.sum(1) == 1);
+        assert!(bit.sum(2) == 3);
         assert!(bit.sum(3) == 6);
     }
 }
